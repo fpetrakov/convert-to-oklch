be086a580026dc87f569d32e35dcc3fe8a6d33c8
fpetrakov <jakephin2015@gmail.com>
2022-08-28 10:37:31 +0300

    add description to --help

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 #!/usr/bin/env node
 const { program } = require("commander");
+const packageVersion = require("../package.json").version;
 const postcss = require("postcss");
 const fs = require("fs");
 const plugin = require("./plugin");
 
+program
+  .name("convert-to-oklch")
+  .description(
+    "CLI tool that converts rgb(), rgba(), hex, hsl() and hsla() colors to oklch() in specified CSS files.",
+  )
+  .version(packageVersion);
+
 program.argument("<path>", "path to css files").action(async (path) => {
-  const css = fs.readFileSync(path, 'utf-8');
+  const css = fs.readFileSync(path, "utf-8");
 
   const result = await postcss([plugin])
     .process(css, { from: path })

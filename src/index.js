653a93eed8e862c4e04976d74fd478f9e3718f34
fpetrakov <jakephin2015@gmail.com>
2022-08-30 11:04:35 +0300

    fix multiple files handling

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const { program } = require("commander");
 const packageVersion = require("../package.json").version;
 const postcss = require("postcss");
 const fs = require("fs");
-const pc = require("picocolors")
+const pc = require("picocolors");
 const plugin = require("./plugin");
 
 program
@@ -13,9 +13,14 @@ program
   )
   .version(packageVersion);
 
-program.argument("<path>", "path to css files").action(async (path) => {
+program.argument("<path>", "path to css files");
+program.parse();
+
+const { args } = program;
+
+const convertColors = async (path) => {
   if (!fs.existsSync(path)) {
-    console.error(pc.bgRed("File doesn't exist"));
+    console.error(pc.bgRed(pc.bgBlack("File doesn't exist")));
     process.exit(1);
   }
 
@@ -28,8 +33,10 @@ program.argument("<path>", "path to css files").action(async (path) => {
   await fs.writeFile(path, result, (err) => {
     if (err) console.error(pc.bgRed(err));
 
-    console.log(pc.bgGreen("Done!"));
+    console.log(pc.bgGreen(pc.black("Done! " + path)));
   });
-});
+};
 
-program.parse();
+for (const path of args) {
+  convertColors(path);
+}

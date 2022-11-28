59e250a05f966636c292c9a5373dc9892bed16bd
fpetrakov <jakephin2015@gmail.com>
2022-11-28 15:30:59 +0300

    refactor plugin a bit

--- src/plugin.js
+++ src/plugin.js
@@ -5,46 +5,46 @@ const pc = require("picocolors");
 module.exports = () => ({
 	postcssPlugin: "postcss-convert-to-oklch",
 	Declaration(decl) {
-		const originalColors = decl.value.match(colorsRegex);
-		if (!originalColors) return;
-
-		const colorsToConvert = originalColors.filter(
-			(originalColor) => !originalColor.includes("var(--"),
-		);
-		if (!colorsToConvert.length) return;
-
-		const convertedAndOriginalColors = colorsToConvert.map(
-			(originalColor) => {
-				try {
-					const converted = getConvertedColor(originalColor);
-					return { converted, original: originalColor };
-				} catch (e) {
-					console.error(
-						pc.bgRed(
-							`Error during color ${originalColor} conversion: ${e}. It will not be converted.`,
-						),
-					);
-				}
-			},
-		);
-		if (!convertedAndOriginalColors.length) return;
-
-		convertedAndOriginalColors.forEach(({ converted, original }) => {
-			try {
-				decl.value = decl.value.replaceAll(original, converted);
-			} catch (e) {
-				console.error(
-					pc.bgRed(
-						`Error during replacing ${original} with ${converted}: ${e}`,
-					),
-				);
-			}
-		});
+		processDecl(decl);
 	},
 });
 
+function processDecl(decl) {
+	const originalColors = decl.value.match(colorsRegex);
+	if (!originalColors) return;
+
+	const colorsToConvert = originalColors.filter(
+		(originalColor) => !originalColor.includes("var(--"),
+	);
+	if (!colorsToConvert.length) return;
+
+	const convertedAndOriginalColors = colorsToConvert.map((originalColor) => {
+		try {
+			const converted = getConvertedColor(originalColor);
+			return { converted, original: originalColor };
+		} catch (e) {
+			logError(e);
+		}
+	});
+	if (!convertedAndOriginalColors.length) return;
+
+	convertedAndOriginalColors.forEach(({ converted, original }) => {
+		try {
+			decl.value = decl.value.replaceAll(original, converted);
+		} catch (e) {
+			logError(e);
+		}
+	});
+}
+
 function getConvertedColor(color) {
 	return new Color(color).to("oklch").toString();
+}
+
+function logError(error) {
+	console.error(
+		pc.bgRed(`Error during replacing ${original} with ${converted}: ${e}`),
+	);
 }
 
 module.exports.postcss = true;

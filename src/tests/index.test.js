c95466453e989d22a8a5006b7807472128bdcfb0
fpetrakov <jakephin2015@gmail.com>
2022-12-06 19:38:13 +0300

    format test files by prettier

--- src/tests/index.test.js
+++ src/tests/index.test.js
@@ -1,17 +1,23 @@
 const postcss = require("postcss");
 const fs = require("fs");
-const path = require('path')
+const path = require("path");
 
 const plugin = require("../plugin");
 
 it("works", async () => {
-  const input = fs.readFileSync(path.join(__dirname, "./fixtures/input.css"), 'utf-8');
-  const output = fs.readFileSync(path.join(__dirname, "./fixtures/output.css"), 'utf-8');
+	const input = fs.readFileSync(
+		path.join(__dirname, "./fixtures/input.css"),
+		"utf-8",
+	);
+	const output = fs.readFileSync(
+		path.join(__dirname, "./fixtures/output.css"),
+		"utf-8",
+	);
 
-  let result = await postcss([plugin]).process(input, {
-    from: undefined,
-  });
+	let result = await postcss([plugin]).process(input, {
+		from: undefined,
+	});
 
-  expect(result.css).toEqual(output);
-  expect(result.warnings()).toHaveLength(0);
+	expect(result.css).toEqual(output);
+	expect(result.warnings()).toHaveLength(0);
 });

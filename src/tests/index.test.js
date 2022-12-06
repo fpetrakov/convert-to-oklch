fe5e2ddcfbfdfdb77746506e9a5ae91f68fc6fa1
fpetrakov <jakephin2015@gmail.com>
2022-12-06 20:26:37 +0300

    add precision tests

--- src/tests/index.test.js
+++ src/tests/index.test.js
@@ -4,20 +4,29 @@ const path = require("path");
 
 const plugin = require("../plugin");
 
-it("works", async () => {
-	const input = fs.readFileSync(
-		path.join(__dirname, "./fixtures/input.css"),
-		"utf-8",
-	);
-	const output = fs.readFileSync(
-		path.join(__dirname, "./fixtures/output.css"),
-		"utf-8",
-	);
-
-	let result = await postcss([plugin]).process(input, {
-		from: undefined,
-	});
-
+it("works with default precision", async () => {
+	const input = readFixture("input.css");
+	const output = readFixture("output.css");
+	const result = await getResult(input, 5);
 	expect(result.css).toEqual(output);
 	expect(result.warnings()).toHaveLength(0);
 });
+
+it("works with precision 3", async () => {
+	const input = readFixture("precision-3-input.css");
+	const output = readFixture("precision-3-output.css");
+	const result = await getResult(input, 3);
+	expect(result.css).toEqual(output);
+	expect(result.warnings()).toHaveLength(0);
+});
+
+function readFixture(name) {
+	const fixturePath = path.join(__dirname, "fixtures", name);
+	return fs.readFileSync(fixturePath, "utf-8");
+}
+
+async function getResult(input, precision) {
+	return await postcss([plugin(precision)]).process(input, {
+		from: undefined,
+	});
+}

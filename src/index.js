57d3cd50247fe60300ee4551e905ce54a74a4a16
fpetrakov <jakephin2015@gmail.com>
2023-02-10 14:38:07 +0300

    simplify things

--- src/index.js
+++ src/index.js
@@ -1,63 +1,57 @@
 #!/usr/bin/env node
-const { program, Option } = require("commander");
-const packageVersion = require("../package.json").version;
-const postcss = require("postcss");
-const fs = require("fs");
-const pc = require("picocolors");
-const plugin = require("./plugin");
-const { logError } = require("./utils");
+const { program, Option } = require('commander')
+const packageVersion = require('../package.json').version
+const postcss = require('postcss')
+const fs = require('fs')
+const pc = require('picocolors')
+const plugin = require('./plugin')
+const { logError } = require('./utils')
 
-const PRECISION_VALUES = Array.from(Array(21), (_, index) => String(index + 1));
+const PRECISION_VALUES = Array.from(Array(21), (_, index) => String(index + 1))
 
 const precisionOption = new Option(
-	"-p, --precision <number>",
-	"precision of color conversion",
-	"5",
-).choices(PRECISION_VALUES);
+	'-p, --precision <number>',
+	'precision of color conversion',
+	'5',
+).choices(PRECISION_VALUES)
 
 program
-	.name("convert-to-oklch")
+	.name('convert-to-oklch')
 	.description(
-		"CLI tool that converts rgb(), rgba(), hex, hsl() and hsla() colors to oklch() in specified CSS files.",
+		'CLI tool that converts rgb(), rgba(), hex, hsl() and hsla() colors to oklch() in specified CSS files.',
 	)
 	.version(packageVersion)
-	.argument("<path>", "path to css files")
+	.argument('<path>', 'path to css files')
 	.addOption(precisionOption)
-	.parse();
+	.parse()
 
-const { args: cssFilePaths } = program;
-const { precision } = program.opts();
+const { args: cssFilePaths } = program
+const { precision } = program.opts()
 
-processFiles();
-
-async function processFiles() {
-	for (const file of cssFilePaths) {
-		await processCssFile(file);
-	}
-
-	console.log(pc.bgGreen(pc.black("Done!")));
-}
-
-async function processCssFile(path) {
+const processCssFile = async path => {
 	if (!fs.existsSync(path)) {
-		logError("File doesn't exist: " + path);
-		return;
+		logError("File doesn't exist: " + path)
+		return
 	}
 
-	const css = fs.readFileSync(path, "utf-8");
+	const css = fs.readFileSync(path, 'utf-8')
 
-	const convertedCss = await getConvertedCss(css, path);
-	await replaceCssFiles(path, convertedCss);
-}
-
-async function getConvertedCss(css, path) {
-	return await postcss([plugin(precision)])
+	const convertedCss = await postcss([plugin(precision)])
 		.process(css, { from: path })
-		.toString();
+		.toString()
+
+	fs.writeFile(path, convertedCss, err => {
+		if (err) {
+			logError(err)
+			fs.writeFileSync(path, css)
+		}
+	})
 }
 
-async function replaceCssFiles(cssFilePath, convertedCss) {
-	fs.writeFile(cssFilePath, convertedCss, (err) => {
-		if (err) logError(err);
-	});
+const processFiles = async () => {
+	cssFilePaths.map(file => processCssFile(file))
+	await Promise.allSettled(cssFilePaths)
+	console.log(pc.bgGreen(pc.black('Done!')))
 }
+
+processFiles()
